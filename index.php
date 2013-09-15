a9b8308fdb8d0f891345a72e78ce33289e4281b3
Oliver <olivermahoney@hotmail.com>
2013-09-15 14:01:02 +0100

    Create index.php
    Basic index.php file for introduction to the library. More to come here for instructions on use and examples. Styles to be initialised.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>MYSQLI Made Easy</title>
+    </head>
+    <body>
+        <h2>MySQLI Made Easy</h2>
+        <small>By Oliver Mahoney</small>
+        <nav>
+            <a href="/LibraryFiles/connect.php">Connect Class De-Bugging</a>
+            <a href="/LibraryFiles/insert.php">Insert Class De-Bugging</a>
+        </nav>
+    </body>
+</html>
