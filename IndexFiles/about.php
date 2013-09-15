d7c3986213b31a7262c9b063a32b4c91d52b3154
Oliver <olivermahoney@hotmail.com>
2013-09-15 15:01:18 +0100

    Create about.php
    Basic about page template

---
+++ IndexFiles/about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>MYSQLIEasy</title>
+        <link rel="stylesheet" href="main.css" />
+    </head>
+    <body>
+        <div class="ContentWrapper">
+            <header>
+                <h2 class="mainHeader">MYSQLIEasy</h2>
+                <nav>
+                    <a href="../index.php">Home</a>
+                    <a href="about.php">About</a>
+                    <a href="instructions.php">Using this Library</a>
+                </nav>
+            </header>
+            <div class="content">
+                <h3 class="secondaryHeader">About This Project...</h3>
+            </div>
+            <footer>
+                <small>An Oliver Mahoney Project</small>
+            </footer>
+        </div>
+    </body>
+</html>
+
